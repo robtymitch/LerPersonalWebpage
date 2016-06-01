76ea6baa7fafc1b582d67033f6c2633aca07f5bf
Tim Selaty <timselaty@gmail.com>
2016-06-01 13:27:14 -0400

    Introduce jQuery & background scrolling

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$.fn.followTo = function () {
+  var $this = this,
+      $window = $(window),
+      pos = 60;
+
+  $this.css({
+    'background-position': 'center ' + pos + 'px',
+    'background-attachment': 'scroll'
+  });
+
+  $window.scroll(function (e) {
+    if ($window.scrollTop() > pos) {
+      $this.css({
+        'background-position': 'center 0px',
+        'background-attachment': 'fixed'
+      });
+    } else {
+      $this.css({
+        'background-position': 'center ' + pos + 'px',
+        'background-attachment': 'scroll'
+      });
+    }
+  });
+};
+
+jQuery(document).ready(function() {
+    $('body').followTo();
+});
